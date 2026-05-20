d9bc772cfb02d51c4985ab96fad170575fad791b
zainan4935 <zainab.khan.4935@gmail.com>
2026-05-20 22:23:06 +0530

    new code

--- aisales.py
+++ aisales.py
@@ -1,2 +1,108 @@
-# this my first project
-print("hello world")
+#Importing Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+#Load Dataset
+
+df = pd.read_csv("data/Sample - Superstore.csv")
+df.head()
+
+df.shape
+df.info()
+df.describe()
+
+# data cleaning
+
+# Check missing values
+df.isnull().sum()
+
+# Remove duplicates
+df.drop_duplicates(inplace=True)
+
+# Convert date column
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+df['Ship Date'] = pd.to_datetime(df['Ship Date'])
+
+df['Year'] = df['Order Date'].dt.year
+df['Month'] = df['Order Date'].dt.month
+df['Day'] = df['Order Date'].dt.day
+df['Weekday'] = df['Order Date'].dt.day_name()
+
+#total sale
+sales_by_year = df.groupby('Year')['Sales'].sum()
+
+plt.plot(sales_by_year)
+plt.title("Yearly Sales Trend")
+plt.xlabel("Year")
+plt.ylabel("Sales")
+plt.show()
+
+#monthly sales
+monthly_sales = df.groupby('Month')['Sales'].sum()
+
+plt.plot(monthly_sales)
+plt.title("Monthly Sales Trend")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.show()
+
+
+top_products = df.groupby('Product Name')['Sales'].sum().sort_values(ascending=False).head(10)
+
+top_products.plot(kind='bar')
+plt.title("Top 10 Products by Sales")
+plt.xlabel("Product Name")
+plt.ylabel("Sales")
+plt.xticks(rotation=90)
+plt.show()
+
+# region by sales
+region_sales = df.groupby('Region')['Sales'].sum()
+
+region_sales.plot(kind='bar')
+plt.title("Region-wise Sales")
+plt.xlabel("Region")
+plt.ylabel("Sales")
+plt.show()
+
+features = df[['Year', 'Month']]
+target = df['Sales']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    features, target, test_size=0.2, random_state=42
+)
+
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# model evaluation
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("RMSE:", np.sqrt(mean_squared_error(y_test, y_pred)))
+
+#actual vs 
+
+plt.scatter(y_test, y_pred)
+plt.title("Actual vs Predicted Sales")
+plt.xlabel("Actual Sales")
+plt.ylabel("Predicted Sales")
+plt.show()
+
+future_data = pd.DataFrame({
+    'Year': [2026, 2026, 2026],
+    'Month': [1, 2, 3]
+})
+
+future_sales = model.predict(future_data)
+print(future_sales)
